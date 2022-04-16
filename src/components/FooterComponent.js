d94dec52b40d5988d9f108ff8fe1ecae9f7a8c6a
abdec2 <abdec2@hotmail.com>
2022-04-16 19:05:45 +0300

    footer added

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import nftCalender from './../images/nft-calender.png'
 
 const FooterComponent = () => {
     return (
         <footer className="footer-area p-4">
             <div className="container">
-                <div className="row mb-5">
+                <div className="row align-items-center justify-content-center mb-5">
                     <div className="col-12 col-sm-6 p-4">
                         <div className="logo-footer">
                             <img src="assets/img/logo/logo.png" alt="" />
@@ -12,31 +12,30 @@ const FooterComponent = () => {
                         <p className='my-0'>A world that has been conquered by an evil and villainous wizard. His master plan was to make every newborn baby into spawns of himself with the same evil intentions!</p>
 
                         <ul className="social-icon-list d-flex pl-0" >
-                            <li className='social-icons1'><a href="#"><em className="fab fa-facebook-f"></em></a></li>
-                            <li className='social-icons1'><a href="#"><em className="fab fa-twitter"></em></a></li>
-                            <li className='social-icons1'><a href="#"><em className="fab fa-youtube"></em></a></li>
-                            <li className='social-icons1'><a href="#"><em className="fab fa-github"></em></a></li>
-                            <li className='social-icons1'><a href="#"><em className="fab fa-bitcoin"></em></a></li>
-                            <li className='social-icons1'><a href="#"><em className="fab fa-medium-m"></em></a></li>
+                            <li className='social-icons1'><a href="https://www.facebook.com/profile.php?id=100078709631597" target='blank'><em className="fab fa-facebook-f"></em></a></li>
+                            <li className='social-icons1'><a href="https://twitter.com/evilwizardnft" target='blank'><em className="fab fa-twitter"></em></a></li>
+                            <li className='social-icons1'><a href="https://instagram.com/evilwizardnft" target='blank'><em className="fab fa-instagram"></em></a></li>
                         </ul>
                     </div>
 
                     <div className="col-12 col-sm-6 p-4">
-                        <div className="nftCalender">
-                            <img src="" alt="" />
+                        <div className="nftCalender mx-auto mx-sm-0 ml-sm-auto">
+                            <a href='https://nftcalendar.io/event/the-evil-wizards/' target='blank'> 
+                                <img src={nftCalender} alt="" />
+                            </a>
                         </div>
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-12 col-sm-6">
-                        <div className=" mb-3">
+                <div className="row align-items-center justify-content-center">
+                    <div className="col-12 col-md-6">
+                        <div className="">
                             <div className="">
-                                <p>Copyright &copy; 2022, Evil Wizards</p>
+                                <p className='text-md-left text-center'>Copyright &copy; 2022 - Evil Wizards</p>
                             </div>
                         </div>
                     </div>
-                    <div className='col-12 col-sm-6 poweredby'>
-                        <p className="text-right">Powered by:&nbsp;
+                    <div className='col-12 col-md-6 poweredby'>
+                        <p className="text-md-right text-center">Powered by:&nbsp;
                             <a href="http://www.brdigitech.com" target="blank" className=""><i>Button R Digitech (bRd)</i></a>
                         </p>
                     </div>
